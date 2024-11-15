801d0d15ae521a0d567d8aff428c49d04e298283
theyeetbear <brodyjjoling@gmail.com>
2024-11-14 21:11:09 -0600

    Level One layout done

---
+++ Time Warp/Assets/Scripts/Door.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField]
+    Camera cam;
+    Transform selectedDoor;
+    GameObject dragPointGameobject;
+    int leftDoor = 0;
+    [SerializeField]
+    LayerMask DoorLayer;
+
+    public MeshRenderer doorRenderer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, 20, DoorLayer))
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                selectedDoor = hit.collider.gameObject.transform.parent;
+                Debug.Log("SelectedDoor set:" + selectedDoor);
+            }
+        }
+
+        if (selectedDoor != null)
+        {
+            HingeJoint joint = selectedDoor.GetComponent<HingeJoint>();
+            JointMotor motor = joint.motor;
+
+            if (dragPointGameobject == null)
+            {
+                dragPointGameobject = new GameObject("Ray door");
+                dragPointGameobject.transform.parent = selectedDoor;
+            }
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            dragPointGameobject.transform.position = ray.GetPoint(Vector3.Distance(selectedDoor.position, transform.position));
+            dragPointGameobject.transform.rotation = selectedDoor.rotation;
+
+            float delta = Mathf.Pow(Vector3.Distance(dragPointGameobject.transform.position, selectedDoor.position), 3);
+
+            //checking left vs right
+            if (doorRenderer.localBounds.center.x > selectedDoor.localPosition.x)
+            {
+                leftDoor = -1;
+            }
+            else
+            {
+                leftDoor = 1;
+            }
+
+            float speedMultiplier = 60000;
+            if (Mathf.Abs(selectedDoor.parent.forward.z) > 0.5f)
+            {
+                if (dragPointGameobject.transform.position.x > selectedDoor.position.x)
+                {
+                    motor.targetVelocity = delta * -speedMultiplier * Time.deltaTime * leftDoor;
+                }
+                else
+                {
+                    motor.targetVelocity = delta * speedMultiplier * Time.deltaTime * leftDoor;
+                }
+            }
+            else
+            {
+                if (dragPointGameobject.transform.position.z > selectedDoor.position.z)
+                {
+                    motor.targetVelocity = delta * -speedMultiplier * Time.deltaTime * leftDoor;
+                }
+                else
+                {
+                    motor.targetVelocity = delta * speedMultiplier * Time.deltaTime * leftDoor;
+                }
+            }
+            joint.motor = motor;
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                selectedDoor = null;
+                motor.targetVelocity = 0;
+                joint.motor = motor;
+                Destroy(dragPointGameobject);
+            }
+        }
+    }
+}
--- Time Warp/Assets/Scripts/RandomGravity.cs
+++ Time Warp/Assets/Scripts/RandomGravity.cs
@@ -5,9 +5,9 @@ public class RandomGravity : MonoBehaviour
     private Rigidbody rb;
     private Vector3 randomGravity;
     private float gravityStrength;
-    private float changeTime = Random.Range(3,10);
+    private float changeTime = 5;
     private float timeSinceLastChange = 0f;
-    private float floatStrength = Random.Range(0,12);
+    private float floatStrength = 0;
 
     private bool gravityReset = false;
 
@@ -15,6 +15,7 @@ public class RandomGravity : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
         ChangeGravity();
     }
 
@@ -23,24 +24,27 @@ public class RandomGravity : MonoBehaviour
     {
         if (!gravityReset)
         {
-            rb.AddForce(Vector3.up * floatStrength);
+            if (timeSinceLastChange >= changeTime)
+            {
+                ChangeGravity();
+                timeSinceLastChange = 0f;
+                changeTime = Random.Range(1, 7);
+            }
+            //rb.AddForce(Vector3.up * Random.Range(1,3));
             rb.AddForce(randomGravity, ForceMode.Acceleration);
         }
-        timeSinceLastChange += Time.fixedDeltaTime;
-
-        if (timeSinceLastChange >= changeTime)
+        else
         {
-            ChangeGravity();
-            timeSinceLastChange = 0f;
-            changeTime = Random.Range(1, 7);
+            rb.useGravity = true;
         }
+        timeSinceLastChange += Time.fixedDeltaTime;
 
     }
 
     void ChangeGravity()
     {
-        floatStrength = Random.Range(0, 12);
-        gravityStrength = Random.Range(0.01f, 50f);
+        //floatStrength = Random.Range(0, 12);
+        gravityStrength = Random.Range(0.01f, .5f);
         randomGravity = new Vector3(
             Random.Range(-1f, 1f),
             Random.Range(-1f, 1f),
